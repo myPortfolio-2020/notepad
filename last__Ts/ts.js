21f37990e3bca380aed7298a6adfa11170f65837
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-12-24 19:00:51 +0500

    done

--- last__Ts/ts.js
+++ last__Ts/ts.js
@@ -3,9 +3,51 @@
 // ts is statically type
 // let x =100;
 // x='a'  (X)- Not Allowed
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // js is dynamically type
 // let x =100;
 // x='a'  (yes)- Allowed
 var x = 111111;
 var y = 12345678946876n;
 console.log(x);
+var arr = [1, 2, 3, 4];
+var arrString = ["a", "b", "c"];
+var arryMixed = ["a", "b", "c", 1, 2, 3, 4];
+var undef = undefined;
+// undef = true; (X) once defined as undefined you can change it
+//we can define object as
+var obj = {
+    nm: "raheel",
+    age: 65,
+    isPay: true,
+};
+// OR
+var obj1 = {
+    nm: "raheel",
+    age: 65,
+    // isPay: true,
+};
+var obj2 = __assign({}, obj); // use of spread operator
+console.log("obj2", obj2);
+// obj2 { nm: 'raheel', age: 65, isPay: true }
+//if
+var obj3 = { obj: obj }; // see if no spread operator
+console.log("obj3", obj3);
+//obj3 { obj: { nm: 'raheel', age: 65, isPay: true } }
+var newObj = {
+    nm: "Jamal",
+    age: 100,
+};
+newObj = __assign(__assign({}, newObj), { isPassed: true });
+console.log(newObj);
+//c
--- last__Ts/ts.ts
+++ last__Ts/ts.ts
@@ -11,3 +11,42 @@
 let x: number = 111_111;
 let y: bigint = 12345678946876n;
 console.log(x);
+
+let arr: number[] = [1, 2, 3, 4];
+let arrString: string[] = ["a", "b", "c"];
+let arryMixed: (number | string)[] = ["a", "b", "c", 1, 2, 3, 4];
+
+let undef: undefined = undefined;
+// undef = true; (X) once defined as undefined you can change it
+
+//we can define object as
+
+let obj: object = {
+  nm: "raheel",
+  age: 65,
+  isPay: true,
+};
+// OR
+let obj1: { nm: string; age: number } = {
+  nm: "raheel",
+  age: 65,
+  // isPay: true,
+};
+
+let obj2 = { ...obj }; // use of spread operator
+console.log("obj2", obj2);
+// obj2 { nm: 'raheel', age: 65, isPay: true }
+
+//if
+let obj3 = { obj }; // see if no spread operator
+console.log("obj3", obj3);
+//obj3 { obj: { nm: 'raheel', age: 65, isPay: true } }
+
+let newObj: object = {
+  nm: "Jamal",
+  age: 100,
+};
+
+newObj = { ...newObj, isPassed: true };
+console.log(newObj);
+//c
