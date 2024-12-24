ef6c21ad89f91a0d59fba89fc75792ede6c0b4e9
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2024-12-24 12:33:47 +0500

    ccc

---
+++ last__Ts/ts.ts
@@ -0,0 +1,15 @@
+// Diifernce between ts and js
+//_____________________________________________________________
+// ts is statically type
+// let x =100;
+// x='a'  (X)- Not Allowed
+
+
+// js is dynamically type
+// let x =100;
+// x='a'  (yes)- Allowed
+
+
+
+
+
