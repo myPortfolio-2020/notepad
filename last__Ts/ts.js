5e71cf7fe9931c0fa446218020bb22648168cd73
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-02-20 15:29:16 +0500

    ddd

--- last__Ts/ts.js
+++ last__Ts/ts.js
@@ -1,1 +1,23 @@
 /// https://www.youtube.com/watch?v=rbK-E9xAZdE&list=PLplW4d4HPsEJL29589GDjtHPAi-R01tnJ&index=83
+//object
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var objOne = {
+    hours: 44,
+    success: true,
+};
+objOne = __assign(__assign({}, objOne), { hours: 77 });
+console.log(objOne);
+// any
+var anyType;
+anyType = 44;
+anyType = 'jhkj';
--- last__Ts/ts.ts
+++ last__Ts/ts.ts
@@ -1,1 +1,17 @@
 /// https://www.youtube.com/watch?v=rbK-E9xAZdE&list=PLplW4d4HPsEJL29589GDjtHPAi-R01tnJ&index=83
+//object
+
+let objOne:{hours: number, success:boolean} = {
+  hours: 44,
+  success: true,
+};
+
+objOne = { ...objOne, hours: 77 };
+
+console.log(objOne);
+// any
+
+let anyType : any
+anyType=44
+anyType='jhkj'
+
