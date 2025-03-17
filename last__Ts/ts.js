3921d0753536b6d141c35266ef2fd802eb935878
myPortfolio-2020 <73186521+myPortfolio-2020@users.noreply.github.com>
2025-03-17 16:03:05 +0500

    sdfsd

--- last__Ts/ts.js
+++ last__Ts/ts.js
@@ -1,23 +1,9 @@
 /// https://www.youtube.com/watch?v=rbK-E9xAZdE&list=PLplW4d4HPsEJL29589GDjtHPAi-R01tnJ&index=83
 //object
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var add = function (a, b) { return a + b; };
+var multi = function (a, b) { return a - b; };
+var officer = {
+    sal: 455,
+    nm: 'ooo',
+    phone: 4654 + 66
 };
-var objOne = {
-    hours: 44,
-    success: true,
-};
-objOne = __assign(__assign({}, objOne), { hours: 77 });
-console.log(objOne);
-// any
-var anyType;
-anyType = 44;
-anyType = 'jhkj';
--- last__Ts/ts.ts
+++ last__Ts/ts.ts
@@ -1,17 +1,92 @@
 /// https://www.youtube.com/watch?v=rbK-E9xAZdE&list=PLplW4d4HPsEJL29589GDjtHPAi-R01tnJ&index=83
 //object
 
-let objOne:{hours: number, success:boolean} = {
-  hours: 44,
-  success: true,
-};
+// let x = "try";
+// // const y = 6
+// // let y = 6;
+// // y = y = 6 + 1;
+// // console.log(y);
 
-objOne = { ...objOne, hours: 77 };
+// // let cane: (string | number)[] = ["a", "c", "d", 2];
 
-console.log(objOne);
-// any
+// // let obj: object = {
+// //   nm: "monkey",
+// //   cat: "animal",
+// // };
 
-let anyType : any
-anyType=44
-anyType='jhkj'
+// // let objOne = { ...obj, nm: "lion" };
+// // let objTwo = { obj };
+// // console.log("obj", obj);
+// // console.log("objOne", objOne);
+// // console.log("objTwo", objTwo);
+
+// // let emp
+// // emp = 1
+// // emp = "emp"
+
+// let myTuple: [string, number] = ["egg", 4];
+// console.log(myTuple);
+// myTuple.pop();
+// console.log(myTuple);
+
+// enum Color {
+//   red =5,
+//   green,
+//   blue
+// }
+// console.log(Color.blue)
+
+
+// enum Size {
+//   Large=1,
+//   Med,
+//   small
+// }
+
+// let mySize = Size.Large
+
+// console.log(mySize)
+
+// let notSure:unknown = 'ooo'
+
+// if(typeof(notSure) === 'number'){
+//     notSure.toFixed(5)
+// }else if(typeof(notSure)==='string'){
+//   notSure.length
+// }
+
+interface asFn {
+  (x:number, y:number):number
+}
+
+const add:asFn=(a,b)=>a+b
+const multi:asFn=(a,b)=>a-b
+
+interface User {
+  nm:string,
+  phone:number
+}
+
+interface UserCash {
+  isPaid: boolean
+}
+
+interface Combined extends User{
+  sal:number  
+}
+
+const officer:Combined ={
+  sal:455,
+  nm:'ooo',
+  phone:4654+66
+}
+
+interface Cabnet {
+  mem:string,
+  sal:number
+}
+
+interface Cabnet {
+  des:string
+}
 
